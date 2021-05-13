134c4157631c4627c2a53f90aed9cdf3c5fd5dc4
John Wason <wason@wasontech.com>
2021-05-12 23:44:07 -0400

    Load Jinja2 package in pyodide

--- src/pyri/webui_server/webui_static/webui_bootstrap.js
+++ src/pyri/webui_server/webui_static/webui_bootstrap.js
@@ -1,7 +1,7 @@
 
 async function webui_bootstrap(){
     await languagePluginLoader;
-    await pyodide.loadPackage(["numpy","micropip"]);
+    await pyodide.loadPackage(["numpy","micropip","Jinja2"]);
     const response = await fetch("webui_bootstrap.py", {cache: "no-store"});
     const webui_bootstrap_py = await response.text();
     pyodide.runPython(webui_bootstrap_py)
