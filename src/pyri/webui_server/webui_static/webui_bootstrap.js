428f955f8aac449eab53459daadbacd706bf3bcd
John Wason <wason@wasontech.com>
2024-04-09 20:42:34 -0400

    Load pyyaml pyodide package

--- src/pyri/webui_server/webui_static/webui_bootstrap.js
+++ src/pyri/webui_server/webui_static/webui_bootstrap.js
@@ -1,7 +1,7 @@
 
 async function webui_bootstrap(){
     let pyodide = await loadPyodide();
-    await pyodide.loadPackage(["numpy","micropip","Jinja2","RobotRaconteur"]);
+    await pyodide.loadPackage(["numpy","micropip","Jinja2","RobotRaconteur","pyyaml"]);
     const response = await fetch("webui_bootstrap.py", {cache: "no-store"});
     const webui_bootstrap_py = await response.text();
     pyodide.runPython(webui_bootstrap_py)
